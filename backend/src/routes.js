c109da21c1cfc9670582e2ef02309f4a257d59ce
soaresmessias130 <soaresmessias130@gmail.com>
2020-03-25 20:13:42 -0300

    Login Session DONE

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,14 +2,20 @@ const express = require('express');
 
 const OngController = require('./controllers/OngContoller');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
-
+//SESSION ROUTE
+routes.post('/sessions', SessionController.create);
+//ONG's Routes
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+//Incidents's Routes
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
-
+//Profiles's Routes
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
